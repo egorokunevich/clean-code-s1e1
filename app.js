f04d664dea6ac5bcef75ee1c8d5a190471d72fcf
egorokunevich <mrcraigre@gmail.com>
2024-01-04 22:49:10 +0300

    feat(basic-3.3): implement concise naming

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.querySelector(".todo-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.querySelector(".tasks_todo");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("tasks_done");//completed-tasks
 
 
 //New task list item
